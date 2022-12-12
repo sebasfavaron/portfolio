ae18d2edcc45ec95e3d1c93a62ec103dcf3ddb2d
Sebastian Favaron <sebastianfavaron@gmail.com>
2022-12-12 18:49:22 -0300

    Initial nextjs commit

---
+++ next.config.mjs
@@ -0,0 +1,35 @@
+// @ts-check
+/**
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
+ * This is especially useful for Docker builds.
+ */
+!process.env.SKIP_ENV_VALIDATION && (await import("./src/env/server.mjs"));
+
+/** @type {import("next").NextConfig} */
+const config = {
+  reactStrictMode: true,
+  swcMinify: true,
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
+  images: {
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'tailwindui.com',
+        port: '',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '**',
+      },
+    ],
+  },
+};
+export default config;
---
+++ src/pages/simulations.tsx
@@ -0,0 +1,17 @@
+import { type NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
+            asd
+          </div>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default Home;
